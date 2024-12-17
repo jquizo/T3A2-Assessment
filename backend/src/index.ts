aa4d231a85bb17346f62236917f481a74ea9af24
jquizo <45918580+jquizo@users.noreply.github.com>
2024-12-17 22:15:41 +1100

    added express validator

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -4,6 +4,7 @@ import "dotenv/config";
 import mongoose from "mongoose";
 import userRoutes from "./routes/users";
 
+mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string);
 
 const app = express();
 app.use(express.json());
--- backend/src/routes/users.ts
+++ backend/src/routes/users.ts
@@ -1,11 +1,25 @@
 import express, { Request, Response } from "express";
 import User from "../models/user";
 import jwt from "jsonwebtoken";
+import { check, validationResult } from "express-validator";
 
 const router = express.Router();
 
-
-router.post("/register",async (req: Request, res: Response) => {
+router.post(
+  "/register",
+  [
+    check("firstName", "First Name is required").isString(),
+    check("lastName", "Last Name is required").isString(),
+    check("email", "Email is required").isEmail(),
+    check("password", "Password with 6 or more characters required").isLength({
+      min: 6,
+    }),
+  ],
+  async (req: Request, res: Response) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ message: errors.array() });
+    }
 
     try {
       let user = await User.findOne({
@@ -27,15 +41,12 @@ router.post("/register",async (req: Request, res: Response) => {
         }
       );
 
-      
       res.cookie("auth_token", token, {
         httpOnly: true,
         secure: process.env.NODE_ENV === "production",
         maxAge: 86400000,
       });
-      return res.status(200).send({ message: "User registered!" });
-
-
+      return res.status(200).send({ message: "User registered OK" });
     } catch (error) {
       console.log(error);
       res.status(500).send({ message: "Something went wrong" });
