d90a6e4ef63e5fcad1698a385b61fb354b9acdad
jquizo <45918580+jquizo@users.noreply.github.com>
2024-12-17 21:13:49 +1100

    added footer component

--- frontend/src/layouts/Layout.tsx
+++ frontend/src/layouts/Layout.tsx
@@ -1,12 +1,13 @@
 import Header from "../components/Header";
 import Hero from '../components/Hero';
-
+import Footer from '../components/Footer';
 
 const Layout = () => {
   return (
     <div className="flex flex-col min-h-screen">
       <Header />
       <Hero />
+      <Footer />
     </div>
   );
 };
