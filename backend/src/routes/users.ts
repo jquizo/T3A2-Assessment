582916c829f7daeb7f71106f518110caf5441b42
jquizo <45918580+jquizo@users.noreply.github.com>
2024-12-17 21:43:18 +1100

    added jwt token generation on user registration

--- backend/src/routes/users.ts
+++ backend/src/routes/users.ts
@@ -13,11 +13,27 @@ router.post("/register",async (req: Request, res: Response) => {
       });
 
       if (user) {
-        return res.status(400).json({ message: "User already exists" });
+        return res.status(400).json({ message: "User already registered" });
       }
 
       user = new User(req.body);
       await user.save();
+
+      const token = jwt.sign(
+        { userId: user.id },
+        process.env.JWT_SECRET_KEY as string,
+        {
+          expiresIn: "1d",
+        }
+      );
+
+      
+      res.cookie("auth_token", token, {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === "production",
+        maxAge: 86400000,
+      });
+      return res.status(200).send({ message: "User registered!" });
 
 
     } catch (error) {
