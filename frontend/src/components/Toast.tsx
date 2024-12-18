a096a3afd6ad436ac4b0a7f658e0db72a044487d
jquizo <45918580+jquizo@users.noreply.github.com>
2024-12-19 00:40:54 +1100

    added toast component for showing toast function

---
+++ frontend/src/components/Toast.tsx
@@ -0,0 +1,21 @@
+import { useEffect } from "react";
+
+type ToastProps = {
+    message: string;
+    type: "SUCCESS" | "ERROR";
+};
+
+const Toast = ({ message, type}: ToastProps) => {
+    
+  
+    return (
+      <div >
+        <div className="flex justify-center items-center">
+          <span className="text-lg font-semibold">{message}</span>
+        </div>
+      </div>
+    );
+  };
+  
+  export default Toast;
+  
