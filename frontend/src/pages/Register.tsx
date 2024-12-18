798c441427f0334e2fc11aab26dd51244d4139c2
jquizo <45918580+jquizo@users.noreply.github.com>
2024-12-18 22:24:08 +1100

    added register page

---
+++ frontend/src/pages/Register.tsx
@@ -0,0 +1,17 @@
+
+
+const Register = () => {
+
+
+  return (
+    <form className="flex flex-col gap-5">
+      <h2 className="text-3xl font-bold">Create an Account</h2>
+      <div className="flex flex-col md:flex-row gap-5">
+
+      </div>
+   
+    </form>
+  );
+};
+
+export default Register;
