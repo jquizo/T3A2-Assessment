cd2e926da734c109cfeb90704a46a5ba8de2d3da
jquizo <45918580+jquizo@users.noreply.github.com>
2024-12-19 19:09:20 +1100

    removed some tests added by playwright

--- endtoend-tests/tests/auth.spec.ts
+++ endtoend-tests/tests/auth.spec.ts
@@ -13,14 +13,8 @@ test('allow user to sign in', async ({ page }) => {
     await page.locator("[name=email]").fill("123@test.com");
     // Finds an element with the name of password and fills it 
     await page.locator("[name=password]").fill("password123");
+
+    // Finds button element with name of login and clicks it 
+    await page.getByRole("button", { name: "Login" }).click();
 });
 
-test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
-
-  // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
-});
