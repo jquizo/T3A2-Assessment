41ca80f799bb7a3a615d2c693f250089d3572f35
jquizo <45918580+jquizo@users.noreply.github.com>
2024-12-20 00:00:57 +1100

    added express validator for add hotels route

--- backend/src/routes/my-hotels.ts
+++ backend/src/routes/my-hotels.ts
@@ -4,6 +4,7 @@ import multer from "multer";
 import Hotel from "../models/hotel";
 import { HotelType } from "../models/hotel";
 import verifyToken from "../middleware/auth";
+import { body } from "express-validator";
 const router = express.Router();
 
 const storage = multer.memoryStorage();
@@ -14,7 +15,24 @@ const upload = multer({
     },
   });
 
-router.post("/", verifyToken, upload.array("imageFiles", 6),async (req: Request, res: Response) => {
+router.post("/", 
+    verifyToken, 
+    [
+        body("name").notEmpty().withMessage("Name is required"),
+        body("city").notEmpty().withMessage("City is required"),
+        body("country").notEmpty().withMessage("Country is required"),
+        body("description").notEmpty().withMessage("Description is required"),
+        body("type").notEmpty().withMessage("Hotel type is required"),
+        body("pricePerNight")
+          .notEmpty()
+          .isNumeric()
+          .withMessage("Price per night is required and must be a number"),
+        body("facilities")
+          .notEmpty()
+          .isArray()
+          .withMessage("Facilities are required"),
+      ],
+    upload.array("imageFiles", 6),async (req: Request, res: Response) => {
     try {
         const imageFiles = req.files as Express.Multer.File[];
         const newHotel: HotelType = req.body;
