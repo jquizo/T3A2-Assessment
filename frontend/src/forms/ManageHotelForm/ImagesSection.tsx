b334bfa8b89b3a025aa8f42e7d8d394af3fd8fa2
jquizo <45918580+jquizo@users.noreply.github.com>
2024-12-20 21:42:38 +1100

    added delete functionality for edit hotel photos

--- frontend/src/forms/ManageHotelForm/ImagesSection.tsx
+++ frontend/src/forms/ManageHotelForm/ImagesSection.tsx
@@ -7,9 +7,22 @@ const ImagesSection = () => {
         register,
         formState: { errors },
         watch,
+        setValue,
       } = useFormContext<HotelFormData>();
 
     const existingImageUrls = watch("imageUrls");
+
+    
+  const handleDelete = (
+    event: React.MouseEvent<HTMLButtonElement, MouseEvent>,
+    imageUrl: string
+  ) => {
+    event.preventDefault();
+    setValue(
+      "imageUrls",
+      existingImageUrls.filter((url) => url !== imageUrl)
+    );
+  };
 
     return (
         <div className="pb-4">
@@ -21,6 +34,7 @@ const ImagesSection = () => {
                     <div className="relative group">
                         <img src={url} className="min-h-full object-cover" />
                         <button
+                        onClick={(event) => handleDelete(event, url)}
                         className="absolute inset-0 flex items-center justify-center bg-black bg-opacity-50 opacity-0 group-hover:opacity-100 text-white"
                         >
                         Delete
