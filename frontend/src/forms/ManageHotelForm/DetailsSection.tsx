7fb5098541cbdd01ecd55db905ad5db5eb673fca
jquizo <45918580+jquizo@users.noreply.github.com>
2024-12-20 14:02:18 +1100

    added guests form for manage hotel page

--- frontend/src/forms/ManageHotelForm/DetailsSection.tsx
+++ frontend/src/forms/ManageHotelForm/DetailsSection.tsx
@@ -43,7 +43,29 @@ const HotelDetailsSection = () => {
                 )}
                 </label>
             </div>
-
+            <label className="text-gray-700 text-sm font-bold flex-1">
+                Description
+            <textarea
+                rows={10}
+                className="border rounded w-full py-1 px-2 font-normal"
+                {...register("description", { required: "This field is required" })}
+                ></textarea>
+                {errors.description && (
+                <span className="text-red-500">{errors.description.message}</span>
+                )}
+            </label>
+            <label className="text-gray-700 text-sm font-bold max-w-[50%]">
+                Price Per Night
+                <input
+                type="number"
+                min={1}
+                className="border rounded w-full py-1 px-2 font-normal"
+                {...register("pricePerNight", { required: "This field is required" })}
+                ></input>
+                {errors.pricePerNight && (
+                <span className="text-red-500">{errors.pricePerNight.message}</span>
+                )}
+            </label>
         </div>
     )
 }
---
+++ frontend/src/forms/ManageHotelForm/GuestsSection.tsx
@@ -0,0 +1,12 @@
+
+
+const GuestsSection = () => {
+
+      return (
+        <div>
+           <h2 className="text-2xl font-bold mb-3">Guests</h2>     
+        </div>
+      );
+};
+
+export default GuestsSection;
