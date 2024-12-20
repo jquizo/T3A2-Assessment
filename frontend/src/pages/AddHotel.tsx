b8e203c939b90a32af53a3c90ce2498be5b395be
jquizo <45918580+jquizo@users.noreply.github.com>
2024-12-20 15:21:17 +1100

    add logic to call api fetch request using usemutation

--- frontend/src/pages/AddHotel.tsx
+++ frontend/src/pages/AddHotel.tsx
@@ -1,7 +1,20 @@
 import ManageHotelForm from "../forms/ManageHotelForm/ManageHotelForm";
-
+import { useMutation } from "react-query";
+import { useAppContext } from "../contexts/AppContext";
+import * as apiClient from "../api-client";
 
 const AddHotel = () => {
+    const { showToast } = useAppContext();
+    const { mutate, isLoading } = useMutation(apiClient.addMyHotel, {
+
+        onSuccess: () => {
+          showToast({ message: "Hotel Saved!", type: "SUCCESS" });
+        },
+        onError: () => {
+          showToast({ message: "Error Saving Hotel", type: "ERROR" });
+        },
+      });
+
     return (
         <div><ManageHotelForm/></div>
     )
