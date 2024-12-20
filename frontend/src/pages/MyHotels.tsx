4976a81c54cee83026f7b662bd67b961abc4c98e
jquizo <45918580+jquizo@users.noreply.github.com>
2024-12-20 16:55:09 +1100

    added myhotels page

---
+++ frontend/src/pages/MyHotels.tsx
@@ -0,0 +1,15 @@
+
+
+
+const MyHotels = () => {
+
+    return (
+        <div className="space-y-5">
+            <span className="flex justify-between">
+            <h1 className="text-3xl font-bold">My Hotels</h1>
+            </span>
+        </div>
+    )
+}
+
+export default MyHotels
