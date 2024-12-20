8cff9eae500299a234e6fbd4a5b4ef40e828e049
jquizo <45918580+jquizo@users.noreply.github.com>
2024-12-20 23:25:00 +1100

    changed footer text

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -3,7 +3,7 @@ const Footer = () => {
       <div className="bg-slate-700 py-10">
         <div className="container mx-auto flex justify-between items-center">
           <span className="text-3xl text-white font-bold tracking-tight">
-            Bookme
+            Mern Booking App
           </span>
           <span className="text-white font-bold tracking-tight flex gap-4">
           </span>
